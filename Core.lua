f20497502f6e141fca7fdc9d288342859330dacb
Eyal Solnik <dev@solnik.net>
2015-10-08 11:38:20 +0300

    Update license, version number and website

--- Core.lua
+++ Core.lua
@@ -1,5 +1,5 @@
 --[[
-	Copyright (c) 2014 Eyal Shilony <Lynxium>
+	Copyright (c) 2014 2015 Eyal Solnik <Lynxium>
 
 	Permission is hereby granted, free of charge, to any person obtaining
 	a copy of this software and associated documentation files (the
